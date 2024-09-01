38e0b303758266cf0de93b9f11250db5f70a7faf
Oleksii Reshetniak <all.reshetnyak@gmail.com>
2024-09-01 23:29:58 +0300

    add internet permission

--- lib/screens/webview_screen.dart
+++ lib/screens/webview_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:morfix/constants/app_constants.dart';
-import 'package:morfix/screens/widgets.dart/navigation_controls.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulWidget {
@@ -12,72 +10,37 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   late WebViewController controller;
-  var loadingPercentage = 0;
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
     controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setNavigationDelegate(
         NavigationDelegate(
-          onPageStarted: (url) {
+          onPageStarted: (String url) {
             setState(() {
-              loadingPercentage = 0;
+              isLoading = true;
             });
           },
-          onProgress: (progress) {
+          onPageFinished: (String url) {
             setState(() {
-              loadingPercentage = progress;
-            });
-          },
-          onPageFinished: (url) {
-            setState(() {
-              loadingPercentage = 100;
+              isLoading = false;
             });
           },
         ),
       )
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..addJavaScriptChannel(
-        'SnackBar',
-        onMessageReceived: (message) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(
-                message.message,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          );
-        },
-      )
-      ..loadRequest(Uri.parse(AppConstants.hostName));
+      ..loadRequest(Uri.parse("https://www.morfix.co.il/"));
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        // appBar: AppBar(
-        //     centerTitle: true,
-        //     title: const Text(AppConstants.appTitle),
-        //     actions: [
-        //       NavigationControl(controller: controller),
-        //     ]),
         body: Stack(
           children: [
-            WebViewWidget(
-              controller: controller,
-            ),
-            loadingPercentage < 100
-                ? LinearProgressIndicator(
-                    color: Colors.red,
-                    value: loadingPercentage / 100.0,
-                  )
-                : Container()
+            WebViewWidget(controller: controller),
           ],
         ),
       ),
